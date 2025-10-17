1c34e49ee32da513596e235289d6be34f3b2aace
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-17 09:39:08 -0400

    added character count

--- cp_1.js
+++ cp_1.js
@@ -1,6 +1,18 @@
 const nameInput= document.getElementById("name");
 const emailInput= document.getElementById("email");
 const commentsInput= document.getElementById("comments");
-const submmitButton= document.getElementById("submit");
+const submitButton= document.getElementById("submit");
 
+submitButton.addEventListener("click",(e) => {
+    e.preventDefault();
+   alert("Form Submitted!");
 
+const entry = document.createElement("div");
+entry.textContent= "Feedback received";
+document.getElementById("feedback-display").appendChild(entry);
+});
+
+commentsInput.addEventListener("input", () =>{
+    const count=commentsInput.ariaValueMax.length;
+    console.log("Characters typed", count);
+})
