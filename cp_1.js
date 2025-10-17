928abac74029e2fd50293c20991e58fc91a7008c
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-17 12:39:35 -0400

    fixed character count

--- cp_1.js
+++ cp_1.js
@@ -2,7 +2,7 @@ const form = document.querySelector("#feedback-form");
 const nameInput= document.getElementById("name");
 const emailInput= document.getElementById("email");
 const commentsInput= document.getElementById("comments");
-const submitButton= document.getElementById("submit");
+const submitButton= document.getElementById("submit-btn");
 const charactersCountDisplay= document.getElementById("character-count");
 const feedbackDisplay= document.getElementById("feedback-display");
 
@@ -29,14 +29,16 @@ alert("Form Submitted!");
 
 });
 
+function updateTotalChars() {
+    const total = nameInput.value.length + emailInput.value.length + commentsInput.value.length;
+    charactersCountDisplay.textContent = `${total} characters`;
+}
 
-form.addEventListener("input", (e)=>{
-    if (e.target.id ==="comments"){
-        charactersCountDisplay.textContent= `${e.target.value.length} characters`;
-        console.log("Characters typed (delegated):", e.target.value.length);
-
-    }
+[nameInput, emailInput, commentsInput].forEach(field => {
+    field.addEventListener("input", updateTotalChars);
 });
+
+
 form.addEventListener("mouseover",(e) =>{
     if(["name", "email", "comments"].includes(e.target.id)){
         showTooltip(e.target);
@@ -55,3 +57,24 @@ form.addEventListener("click", (e) => {
         e.stopPropagation();
     }
 });
+
+function showTooltip(field) {
+    const tooltip = document.createElement("div");
+    tooltip.className = "tooltip";
+
+    if (field.id ==="name") tooltip.textContent= "Your full name";
+    if (field.id ==="email") tooltip.textContent= "Your email address";
+    if (field.id ==="comments") tooltip.textContent= "Write your comments here";
+
+    tooltip.style.position = "absolute";
+    tooltip.style.top = field.offsetTop-25+"px";
+    tooltip.style.left = field.offsetLeft+"px";
+
+    document.body.appendChild(tooltip);
+
+}
+
+function hideTooltip(field) {
+    const tooltip= document.querySelector(".tooltip");
+    if(tooltip) tooltip.remove();
+}
