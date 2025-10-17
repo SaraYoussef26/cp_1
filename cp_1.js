7b85302abd1b025024582882ffc63623eb650586
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-17 09:18:39 -0400

    created form layout

---
+++ cp_1.js
@@ -0,0 +1,6 @@
+const nameInput= document.getElementById("name");
+const emailInput= document.getElementById("email");
+const commentsInput= document.getElementById("comments");
+const submmitButton= document.getElementById("submit");
+
+
