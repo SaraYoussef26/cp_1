6f9e6fe74261be94266caaf9006822eea667766e
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-17 10:08:39 -0400

    displayed tooltips

--- cp_1.js
+++ cp_1.js
@@ -2,6 +2,8 @@ const nameInput= document.getElementById("name");
 const emailInput= document.getElementById("email");
 const commentsInput= document.getElementById("comments");
 const submitButton= document.getElementById("submit");
+const charactersCountDisplay= document.getElementById("character-count");
+
 
 submitButton.addEventListener("click",(e) => {
     e.preventDefault();
@@ -13,6 +15,10 @@ document.getElementById("feedback-display").appendChild(entry);
 });
 
 commentsInput.addEventListener("input", () =>{
-    const count=commentsInput.ariaValueMax.length;
+    const count=commentsInput.value.length;
     console.log("Characters typed", count);
-})
+    charactersCountDisplay.textContent= `${count} characters`;
+});
+
+field.addEventListener("mouseover", () => showTooltip());
+field.addEventListener("mouseout", () => hideTooltip());
